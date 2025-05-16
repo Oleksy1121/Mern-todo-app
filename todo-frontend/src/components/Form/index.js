878ead5e025f1a2fdb74d928731ac98cd5236355
Oleksy1121 <oleksy1121@o2.pl>
2025-05-16 18:28:54 +0200

    feat: Implement fetching and creating tasks

--- todo-frontend/src/components/Form/index.js
+++ todo-frontend/src/components/Form/index.js
@@ -1,18 +1,46 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FormContainer, Input, Button } from './styles'
+import axios from '../../axios'
 
 function Form() { 
     const [input, setInput] = useState('')
+    const [todos, setTodos] = useState([])
+
     const getInput = (e) => {
         setInput(e.target.value)
     }
 
-    console.log(input, 'input')
+    const fetchData = async () => {
+        const response = await axios.get('/todos')
+        setTodos(response.data)
+    }
+    
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const addTodo = async (e) => {
+        e.preventDefault()
+        try {
+            await axios.post('/todos', {
+                ...todos,
+                text: input,
+                completed: false
+            })
+        } catch (err) {
+            console.log(err.message)
+        }
+
+        fetchData()
+        setInput('')
+    }
+
+    // console.log(todos, 'todos')
 
   return (
     <FormContainer>
         <Input placeholder='Add new todo' onChange={getInput}></Input>
-        <Button>Add</Button>
+        <Button onClick={addTodo}>Add</Button>
     </FormContainer>
   )
 }
